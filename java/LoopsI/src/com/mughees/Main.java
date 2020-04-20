5ad42485e72c6baa5e949e9b52a69c666d0cd1d7
mughees-asif <mughees460@gmail.com>
2020-04-20 16:35:18 +0100

    LoopsI

--- java/LoopsI/src/com/mughees/Main.java
+++ java/LoopsI/src/com/mughees/Main.java
@@ -1,16 +1,8 @@
 package com.mughees;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 public class Main {
-
-
 
     private static final Scanner scanner = new Scanner(System.in);
 
