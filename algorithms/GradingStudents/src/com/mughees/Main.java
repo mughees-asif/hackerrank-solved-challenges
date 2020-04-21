c303fae1c6fb6b8e81726cfda4baf217d7ae954e
mughees-asif <mughees460@gmail.com>
2020-04-21 16:56:53 +0100

    GradingStudents

--- algorithms/GradingStudents/src/com/mughees/Main.java
+++ algorithms/GradingStudents/src/com/mughees/Main.java
@@ -1,8 +1,32 @@
 package com.mughees;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+
+        Scanner sc = new Scanner(System.in);
+        int numberOfStudents = sc.nextInt();
+        int[] gradesArray = new int[numberOfStudents];
+        for (int i = 0; i < numberOfStudents; i++) {
+            int grade = sc.nextInt();
+            if (grade < 38) {
+                gradesArray[i] = grade;
+            } else {
+                int num1 = grade + 1;
+                int num2 = grade + 2;
+                if (num1 % 5 == 0) {
+                    gradesArray[i] = num1;
+                } else if (num2 % 5 == 0) {
+                    gradesArray[i] = num2;
+                } else {
+                    gradesArray[i] = grade;
+                }
+            }
+        }
+        for (int i = 0; i < numberOfStudents; i++) {
+            System.out.println(gradesArray[i]);
+        }
     }
 }
