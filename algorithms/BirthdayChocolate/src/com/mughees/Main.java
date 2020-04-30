c8b2c1b18d120bc9c2f7369e2ffb1591682c4354
mughees-asif <mughees460@gmail.com>
2020-04-30 18:26:25 +0100

    BirthdayChocolate completed

--- algorithms/BirthdayChocolate/src/com/mughees/Main.java
+++ algorithms/BirthdayChocolate/src/com/mughees/Main.java
@@ -1,8 +1,30 @@
 package com.mughees;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // test case
+        List<Integer> sample = Arrays.asList(4);
+	    birthday(sample, 4 ,1);
+    }
+
+    private static int birthday(List<Integer> s, int d, int m) {
+        int counter = 0;
+        int sum;
+        for(int i = 0; i < s.size() - (m + 1); i++) {
+             sum = s.get(i);
+             int j = i + 1;
+             while (j < (i + m)) {
+                 sum += s.get(j++);
+            }
+             if(sum == d) {
+                 counter++;
+             }
+        }
+        System.out.println(counter);
+        return counter;
     }
 }
