f920b9523918a37c8afe0cc7b0cceb526bdd6cba
mughees-asif <mughees460@gmail.com>
2020-05-01 15:46:05 +0100

    TimeConversion completed

--- algorithms/TimeConversion/src/com/mughees/Main.java
+++ algorithms/TimeConversion/src/com/mughees/Main.java
@@ -3,6 +3,12 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(timeConversion("07:05:45PM"));
+    }
+
+    private static String timeConversion(String s) {
+        int hour = Integer.parseInt(s.substring(0, 2)) % 12;
+        if (s.endsWith("PM")) { hour += 12; }
+        return String.format("%02d", hour) + s.substring(2, 8);
     }
 }
