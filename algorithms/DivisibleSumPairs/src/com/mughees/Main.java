499b0bf53d797d3c07aba12bebb78c889dbd2589
mughees-asif <mughees460@gmail.com>
2020-05-01 16:10:52 +0100

    DivisibleSumPairs completed

--- algorithms/DivisibleSumPairs/src/com/mughees/Main.java
+++ algorithms/DivisibleSumPairs/src/com/mughees/Main.java
@@ -3,6 +3,18 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(divisibleSumPairs(6, 3, new int[]{1, 3, 2, 6, 1, 2}));
+    }
+
+    public static int divisibleSumPairs(int n, int k, int[] ar) {
+        int counter = 0;
+        for (int i = 0; i <= n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if ((ar[i] + ar[j]) % k == 0) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
     }
 }
