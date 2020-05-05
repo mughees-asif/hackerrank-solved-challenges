cd93d5b04fe883f6514fcd8825d5d58c6c376f7e
mughees-asif <mughees460@gmail.com>
2020-05-05 19:23:31 +0100

    MigratoryBirds

--- algorithms/MigratoryBirds/src/com/mughees/Main.java
+++ algorithms/MigratoryBirds/src/com/mughees/Main.java
@@ -1,8 +1,28 @@
 package com.mughees;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // test case
+        int[] arr = {1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4};
+        List<Integer> list = new LinkedList<Integer>();
+        for (int value : arr) {
+            list.add(value);
+        }
+        migratoryBirds(list);
+    }
+
+    public static int migratoryBirds(List<Integer> arr) {
+        int[] typesOfBirds = new int[arr.size() + 1];
+        int birdType = arr.size();
+        for (Integer bird : arr) {
+            typesOfBirds[bird]++;
+            if (typesOfBirds[birdType] == typesOfBirds[bird] && birdType > bird || typesOfBirds[birdType] < typesOfBirds[bird]) {
+                birdType = bird ;
+            }
+        }
+        return birdType;
     }
 }
