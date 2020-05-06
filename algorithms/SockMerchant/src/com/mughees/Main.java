7facf60bcdd6cfdbbf09abbae6e0b0d2415267ab
mughees-asif <mughees460@gmail.com>
2020-05-06 18:03:32 +0100

    SockMerchant

--- algorithms/SockMerchant/src/com/mughees/Main.java
+++ algorithms/SockMerchant/src/com/mughees/Main.java
@@ -1,8 +1,28 @@
 package com.mughees;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        sockMerchant(9, new int[]{1, 1, 3, 1, 2, 1, 3, 3, 3, 3});
+    }
+
+    public static void sockMerchant(int n, int[] arr) {
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            Integer c = hmap.get(arr[i]);
+            if (hmap.get(arr[i]) == null) hmap.put(arr[i], 1);
+            else hmap.put(arr[i], ++c);
+        }
+        for (Integer i : hmap.keySet()) {
+            Integer cnt = hmap.get(i);
+            if (cnt >= 1) {
+                System.out.println(i + " -> " + cnt);
+            }
+            if (cnt % 2 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
