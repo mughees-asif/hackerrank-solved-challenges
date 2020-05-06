02312ae8c53c52cb4ec211839ed41ed5559d5eeb
mughees-asif <mughees460@gmail.com>
2020-05-06 18:26:03 +0100

    PageCount completed

--- algorithms/PageCount/src/com/mughees/Main.java
+++ algorithms/PageCount/src/com/mughees/Main.java
@@ -3,6 +3,12 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // test case
+        pageCount(6, 2);
+    }
+
+    static int pageCount(int n, int p) {
+        int min = Math.min(p / 2, (n / 2) - (p / 2));
+        return min;
     }
 }
