448556a168ed737d5863dc628ec456d6b69aa42c
mughees-asif <mughees460@gmail.com>
2020-05-06 19:24:18 +0100

    CountingValley completed

--- algorithms/CountingValley/src/com/mughees/Main.java
+++ algorithms/CountingValley/src/com/mughees/Main.java
@@ -3,6 +3,24 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // test case
+        countingValleys(9, "DDUUDDUDUUUD");
+    }
+
+    public static int countingValleys(int n, String s) {
+        int height = 0;
+        int counter = 0;
+        char[] string = s.toCharArray();
+        for (char c : string) {
+            if (c == 'U') {
+                if (height == -1) {
+                    counter++;
+                }
+                height++;
+            } else {
+                height--;
+            }
+        }
+        return counter;
     }
 }
