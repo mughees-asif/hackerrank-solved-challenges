07efecbbf5f77e0f12c684db66772d9f551cb4bf
mughees-asif <mughees460@gmail.com>
2020-05-06 18:14:40 +0100

    SockMerchant completed

--- algorithms/SockMerchant/src/com/mughees/Main.java
+++ algorithms/SockMerchant/src/com/mughees/Main.java
@@ -5,18 +5,19 @@ import java.util.HashSet;
 public class Main {
 
     public static void main(String[] args) {
+        // test case
         sockMerchant(9, new int[]{1, 1, 3, 1, 2, 1, 3, 3, 3, 3});
     }
 
     public static int sockMerchant(int n, int[] arr) {
         HashSet<Integer> hashSet = new HashSet<>();
-        int pairs = 0;
+        int counter = 0;
         for (int value : arr) {
             if (!hashSet.add(value)) {
                 hashSet.remove(value);
-                pairs++;
+                counter++;
             }
         }
-        return pairs;
+        return counter;
     }
 }
