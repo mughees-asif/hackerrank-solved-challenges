d5efc7245c7a4dd0d2ed38b0560fb4ae7c5293ca
mughees-asif <mughees460@gmail.com>
2020-05-11 16:09:25 +0100

    CatAndMouse completed

--- algorithms/CatAndMouse/src/com/mughees/Main.java
+++ algorithms/CatAndMouse/src/com/mughees/Main.java
@@ -3,10 +3,13 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-        catAndMouse(1, 2, 3);
+        System.out.println(catAndMouse(1, 2, 3));
+        System.out.println(catAndMouse(1, 3, 2));
     }
 
     public static String catAndMouse(int x, int y, int z) {
-        if (x > y)
+        int catA = Math.abs(z - x);
+        int catB = Math.abs(z - y);
+        return catA > catB ? "Cat B" : (catA < catB ? "Cat A" : "Mouse C");
     }
 }
