4d7b8826732391cdf3661cda6094b71265319513
mughees-asif <mughees460@gmail.com>
2020-05-13 18:19:07 +0100

    CloudJump completed

--- algorithms/CloudJump/src/com/mughees/Main.java
+++ algorithms/CloudJump/src/com/mughees/Main.java
@@ -1,8 +1,27 @@
 package com.mughees;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int[] c = new int[n];
+        for (int c_i = 0; c_i < n; c_i++) {
+            c[c_i] = scanner.nextInt();
+        }
+
+        int energyLevel = 100;
+        int cloud = 0;
+        do {
+            energyLevel--;       // performed a jump
+            cloud = (cloud + k) % n;
+            if (c[cloud] == 1) {
+                energyLevel -= 2;     // landed on a thundercloud
+            }
+        } while (cloud != 0);
+        System.out.println(energyLevel);
     }
 }
