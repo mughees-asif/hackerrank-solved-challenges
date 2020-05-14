29568804a9def918cdf9cd86e16d60fb8f2bef9c
mughees-asif <mughees460@gmail.com>
2020-05-14 18:26:46 +0100

    DesignerPDFViewer completed

--- algorithms/DesignerPDFViewer/src/com/mughees/Main.java
+++ algorithms/DesignerPDFViewer/src/com/mughees/Main.java
@@ -3,6 +3,18 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] test = {1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5,
+                5, 5, 5, 5, 5, 5, 5, 5, 5, 7};
+        System.out.println(designerPdfViewer(test, "zaba"));
+    }
+
+    public static int designerPdfViewer(int[] h, String word) {
+        int stringWidth = word.length();
+        int maxHeight = 0;
+        for(int i = 0; i < stringWidth; i++) {
+            char tempChar = word.charAt(i);
+            maxHeight = Math.max(maxHeight, h[tempChar - (int)'a']);
+        }
+        return maxHeight * stringWidth;
     }
 }
