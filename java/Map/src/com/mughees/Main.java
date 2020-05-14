4a31524ac7d0f2fd15f0f04b1e992c2696a13139
mughees-asif <mughees460@gmail.com>
2020-05-14 18:36:23 +0100

    Map

---
+++ java/Map/src/com/mughees/Main.java
@@ -0,0 +1,20 @@
+package com.mughees;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine();
+        for (int i = 0; i < n; i++) {
+            String name = in.nextLine();
+            int phone = in.nextInt();
+            in.nextLine();
+        }
+        while (in.hasNext()) {
+            String s = in.nextLine();
+        }
+    }
+}
