f8945907dd089ec218349868752291db37e3c8ba
mughees-asif <mughees460@gmail.com>
2020-05-14 17:19:49 +0100

    HurdleRace completed

--- algorithms/HurdleRace/src/com/mughees/Main.java
+++ algorithms/HurdleRace/src/com/mughees/Main.java
@@ -1,8 +1,17 @@
 package com.mughees;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int[] test = {2,5,4,5,2};
+        System.out.println(hurdleRace(test, 7));
+    }
+
+    public static int hurdleRace(int[] height, int k) {
+        Arrays.sort(height);
+        int minDose = height[height.length - 1] - k;
+        return Math.max(minDose, 0);
     }
 }
