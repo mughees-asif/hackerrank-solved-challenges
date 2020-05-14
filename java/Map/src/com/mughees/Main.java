38fbc7cee715bac08367a0617123931ba06197de
mughees-asif <mughees460@gmail.com>
2020-05-14 19:02:30 +0100

    Map completed

--- java/Map/src/com/mughees/Main.java
+++ java/Map/src/com/mughees/Main.java
@@ -1,20 +1,29 @@
 package com.mughees;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        String name, query, phone;
+        Map<String, String> map = new HashMap<>();
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         in.nextLine();
         for (int i = 0; i < n; i++) {
-            String name = in.nextLine();
-            int phone = in.nextInt();
-            in.nextLine();
+            name = in.nextLine();
+            phone = in.nextLine();
+            map.put(name, phone);
         }
         while (in.hasNext()) {
-            String s = in.nextLine();
+            query = in.nextLine();
+            if(map.containsKey(query)) {
+                System.out.println(query+"="+map.get(query));
+            } else {
+                System.out.println("Not found");
+            }
         }
     }
 }
