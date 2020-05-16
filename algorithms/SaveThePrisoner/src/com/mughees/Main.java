8bee3a58be4217a430b58ec41818c9891fe3182f
mughees-asif <mughees460@gmail.com>
2020-05-16 18:23:15 +0100

    SaveThePrisoner

--- algorithms/SaveThePrisoner/src/com/mughees/Main.java
+++ algorithms/SaveThePrisoner/src/com/mughees/Main.java
@@ -3,6 +3,11 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(saveThePrisoner(5,2,2));
+    }
+
+    static int saveThePrisoner(int n, int s, int m) {
+        int unluckyPrisoner = (s + (m - 1)) % n;
+        return (unluckyPrisoner == 0) ? n : unluckyPrisoner;
     }
 }
