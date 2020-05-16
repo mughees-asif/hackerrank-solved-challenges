9dd49b30fc6dbcd0f6f96bae87a24bc6f4c2739b
mughees-asif <mughees460@gmail.com>
2020-05-16 20:27:22 +0100

    CircularArray compelted

--- algorithms/CircularArray/src/com/mughees/Main.java
+++ algorithms/CircularArray/src/com/mughees/Main.java
@@ -1,8 +1,21 @@
 package com.mughees;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] test = {3,4,5};
+        int[] query = {1,2};
+        System.out.println(Arrays.toString(circularArrayRotation(test, 2, query)));
+    }
+
+    static int[] circularArrayRotation(int[] a, int k, int[] queries) {
+        k %= a.length;
+        int[] result = new int[queries.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = a[(queries[i] - k + a.length) % a.length];
+        }
+        return result;
     }
 }
