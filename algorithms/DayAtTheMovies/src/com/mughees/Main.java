db498e479135d93a181862e4e55b76af04138bf9
mughees-asif <mughees460@gmail.com>
2020-05-17 18:46:17 +0100

    DayAtTheMovies completed

--- algorithms/DayAtTheMovies/src/com/mughees/Main.java
+++ algorithms/DayAtTheMovies/src/com/mughees/Main.java
@@ -1,8 +1,19 @@
 package com.mughees;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(beautifulDays(23, 20, 6));
+    }
+
+    static int beautifulDays(int i, int j, int k) {
+        int beautifulDays = 0;
+        for(int start = i; start <= j; start++) {
+            if(Math.abs(start - Integer.parseInt(new StringBuilder().append(start).reverse().toString())) % k == 0)
+                beautifulDays++;
+        }
+        return beautifulDays;
     }
 }
