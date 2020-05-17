6815e4abd9b1257bfa45276c6e41ed5431c1ca36
mughees-asif <mughees460@gmail.com>
2020-05-17 19:19:44 +0100

    ViralAdvertising completed

--- algorithms/ViralAdvertising/src/com/mughees/Main.java
+++ algorithms/ViralAdvertising/src/com/mughees/Main.java
@@ -3,6 +3,15 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(viralAdvertising(4));
+    }
+
+    static int viralAdvertising(int n) {
+        int peopleReached = 2, peopleSharing = 2;
+        for(int i = 1; i < n; i++) {
+            peopleReached += (peopleSharing * 3) / 2;
+            peopleSharing = (peopleSharing * 3) / 2;
+        }
+        return peopleReached;
     }
 }
