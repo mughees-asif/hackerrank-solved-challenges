4ac9eb122971f0a0b5e46afbcc4da4862e6cd562
mughees-asif <mughees460@gmail.com>
2020-05-22 17:59:11 +0100

    BeautifulTriplets completed

--- algorithms/BeautifulTriplets/src/com/mughees/Main.java
+++ algorithms/BeautifulTriplets/src/com/mughees/Main.java
@@ -1,8 +1,22 @@
 package com.mughees;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] arr = {2, 2, 3, 4, 5};
+        System.out.println(beautifulTriplets(1, arr));
+    }
+
+    static int beautifulTriplets(int d, int[] arr) {
+        int beautifulTriplets = 0;
+        for (int value : arr) {
+            if (Arrays.binarySearch(arr, value + d) >= 0 &&
+                    Arrays.binarySearch(arr, value + d * 2) >= 0) {
+                ++beautifulTriplets;
+            }2
+        }
+        return beautifulTriplets;
     }
 }
