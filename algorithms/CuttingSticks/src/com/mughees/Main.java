abfc850bbea9175abb4e40a6b8c4dfec6dd80134
mughees-asif <mughees460@gmail.com>
2020-05-24 17:11:06 +0100

    CuttingTheSticks

---
+++ algorithms/CuttingSticks/src/com/mughees/Main.java
@@ -0,0 +1,34 @@
+package com.mughees;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int lengths[] = new int[n];
+        List<Integer> output = new LinkedList<>();
+
+        for (int i = 0; i < n; i++)
+            lengths[i] = sc.nextInt();
+
+        sc.close();
+        Arrays.sort(lengths);
+
+        for (int i = 0; i < n; i++) {
+            if (lengths[i] == 0)
+                continue;
+            else {
+                output.add(n - i);
+                final int v = lengths[i];
+                for (int j = i; j < n; j++) {
+                    lengths[j] -= v;
+                }
+            }
+        }
+        for (int i = 0; i < output.size(); i++)
+            System.out.println(output.get(i));
+    }
+}
