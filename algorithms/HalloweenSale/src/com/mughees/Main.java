d0fbe9e2eb60c1bb157fc37e69a6937cc6f52fc5
mughees-asif <mughees460@gmail.com>
2020-05-24 19:25:41 +0100

    HalloweenSale completed

--- algorithms/HalloweenSale/src/com/mughees/Main.java
+++ algorithms/HalloweenSale/src/com/mughees/Main.java
@@ -1,8 +1,21 @@
 package com.mughees;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(howManyGames(20, 3,6,80));
+    }
+
+    static int howManyGames(int p, int d, int m, int s) {
+        int price = p;
+        int result = 0;
+        while (s >= price) {
+            s -= price;
+            result++;
+            price = Math.max(m, price - d);
+        }
+        return result;
     }
 }
