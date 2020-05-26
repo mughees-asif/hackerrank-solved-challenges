7852cbdcf7e826a5236b773bcc2a471ba2d938be
mughees-asif <mughees460@gmail.com>
2020-05-26 15:48:35 +0100

    Java Regex

--- java/Regex/src/com/mughees/Main.java
+++ java/Regex/src/com/mughees/Main.java
@@ -1,8 +1,24 @@
 package com.mughees;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public static final String regularExpression = "[a-zA-Z][a-zA-Z0-9_]{7,29}$";
+
+    private static final Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-	    
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(Main.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
     }
 }
+
