bb88ffaea15b9e36506ed4d72eeac15563a649db
mughees-asif <mughees460@gmail.com>
2020-05-26 16:24:19 +0100

    1DArrayII

---
+++ java/1DArrayII/src/com/mughees/Main.java
@@ -0,0 +1,12 @@
+package com.mughees;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static boolean canWin(int leap, int[] game) {
+        // Return true if you can win the game; otherwise, return false.
+    }
+}
