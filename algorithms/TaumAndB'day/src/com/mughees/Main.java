d31aa678dad53aa7c12af2653d0a94421636393d
mughees-asif <mughees460@gmail.com>
2020-05-26 17:12:36 +0100

    TaumAndB'day completed

--- algorithms/TaumAndB'day/src/com/mughees/Main.java
+++ algorithms/TaumAndB'day/src/com/mughees/Main.java
@@ -3,6 +3,27 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(taumBday(10, 10, 1, 1, 1));
+        System.out.println(taumBday(5, 9, 2, 3, 4));
+        System.out.println(taumBday(3, 6, 9, 1, 1));
+        System.out.println(taumBday(7, 7, 4, 2, 1));
+    }
+
+    /*
+     * Complete the 'taumBday' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER b
+     *  2. INTEGER w
+     *  3. INTEGER bc
+     *  4. INTEGER wc
+     *  5. INTEGER z
+     */
+
+    public static long taumBday(int b, int w, int bc, int wc, int z) {
+        long priceB = Math.min(bc, wc + z);
+        long priceW = Math.min(wc, bc + z);
+        return (b * priceB) + (w * priceW);
     }
 }
