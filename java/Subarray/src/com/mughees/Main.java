2a0a1487c8b84a8ab2b0f555d903346c3e2ba960
mughees-asif <mughees460@gmail.com>
2020-05-26 16:09:24 +0100

    Subarrays

--- java/Subarray/src/com/mughees/Main.java
+++ java/Subarray/src/com/mughees/Main.java
@@ -1,8 +1,25 @@
 package com.mughees;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int count = 0, sum = 0;
+        int input = scanner.nextInt();
+        int[] inputArray = new int[input];
+        for(int i = 0; i < input; i++) {
+            inputArray[i] = scanner.nextInt();
+        }
+        scanner.close();
+        for(int i = 0; i < inputArray.length; i++) {
+            for(int j = i; j < inputArray.length; j++) {
+                sum += inputArray[j];
+                if(sum < 0)     count++;
+            }
+        }
+        System.out.println("count= "+count);
     }
 }
