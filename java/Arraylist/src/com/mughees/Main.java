fbcfe2e3f3cc51fd8a255855aed0c977cc434e64
mughees-asif <mughees460@gmail.com>
2020-05-26 16:21:29 +0100

    Arraylist completed

--- java/Arraylist/src/com/mughees/Main.java
+++ java/Arraylist/src/com/mughees/Main.java
@@ -1,8 +1,36 @@
 package com.mughees;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+
+        List<List<Integer>> lines = new ArrayList<>();
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            List<Integer> line = new ArrayList<>();
+            int d = in.nextInt();
+            for (int j = 0; j < d; j++) {
+                line.add(in.nextInt());
+            }
+            lines.add(line);
+        }
+
+        int q = in.nextInt();
+        for (int i = 0; i < q; i++) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            if (y > lines.get(x - 1).size()) {
+                System.out.println("ERROR!");
+            } else {
+                System.out.println(lines.get(x - 1).get(y - 1));
+            }
+        }
+
+        in.close();
     }
 }
