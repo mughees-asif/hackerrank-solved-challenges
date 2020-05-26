4f65e84e1f16f00a55529ff894dde00597159589
mughees-asif <mughees460@gmail.com>
2020-05-26 16:42:43 +0100

    1DArrayII completed

--- java/1DArrayII/src/com/mughees/Main.java
+++ java/1DArrayII/src/com/mughees/Main.java
@@ -3,10 +3,23 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(canWin(3, new int[]{0,0,1,1,1,0}));
+        System.out.println(canWin(3, new int[]{0,0,0,0,0}));
+        System.out.println(canWin(3, new int[]{0,0,0,1,1,1}));
+
     }
 
     public static boolean canWin(int leap, int[] game) {
-        // Return true if you can win the game; otherwise, return false.
+        return isSolvable(leap, game, 0);
+    }
+
+    public static boolean isSolvable(int leap, int[] game, int i) {
+        if(i >= game.length)    return true;
+        if(i < 0 || game[i] == 1)   return false;
+        game[i] = 1;
+
+        return isSolvable(leap, game, i+leap)
+                || isSolvable(leap, game, i+1)
+                || isSolvable(leap, game, i-1);
     }
 }
