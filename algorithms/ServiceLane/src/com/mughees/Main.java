ea0d44e67e70c23f3882e3c56abdb188a43de839
mughees-asif <mughees460@gmail.com>
2020-06-12 15:09:26 +0100

    ServiceLane!

--- algorithms/ServiceLane/src/com/mughees/Main.java
+++ algorithms/ServiceLane/src/com/mughees/Main.java
@@ -1,8 +1,30 @@
 package com.mughees;
 
-public class Main {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int t = scanner.nextInt();
+        int[] sortedArray;
+        int[] result = new int[t];
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        for(int i = 0; i < t; i++) {
+            int firstRangeNumber = scanner.nextInt();
+            int secondRangeNumber = scanner.nextInt();
+            sortedArray = Arrays.copyOfRange(arr, firstRangeNumber, secondRangeNumber + 1);
+            Arrays.sort(sortedArray);
+            result[i] = sortedArray[0];
+        }
+        scanner.close();
+        for (int value : result) {
+            System.out.println(value);
+        }
     }
 }
