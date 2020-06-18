bb486ca207b92de261b5b60b4f56869972f938e0
mughees-asif <mughees460@gmail.com>
2020-06-18 11:33:31 +0100

    ManasaStones completed

--- algorithms/ManasaStones/src/com/mughees/Main.java
+++ algorithms/ManasaStones/src/com/mughees/Main.java
@@ -1,8 +1,28 @@
 package com.mughees;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int testCases = scanner.nextInt();
+
+        for (int i = 0; i < testCases; i++) {
+            int nonZeroStones = scanner.nextInt();
+
+            SortedSet<Integer> set = new TreeSet<>();
+            int aDifference = scanner.nextInt();
+            int bDifference = scanner.nextInt();
+
+            for (int k = 0; k < nonZeroStones; k++) {
+				int val = aDifference * (nonZeroStones - 1 - k) + bDifference * k;
+				set.add(val);
+			}
+			for (Object it : set) {
+				System.out.print(it + " ");
+			}
+            System.out.println();
+        }
     }
 }
