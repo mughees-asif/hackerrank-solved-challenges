724490aa07977bc72ad45a92b5e7fd6ae4e2c7e0
mughees-asif <mughees460@gmail.com>
2020-06-19 15:15:11 +0100

    ClimbingTheLeaderboard completed

--- algorithms/ClimbingTheLeaderboard/src/com/mughees/Main.java
+++ algorithms/ClimbingTheLeaderboard/src/com/mughees/Main.java
@@ -1,8 +1,31 @@
 package com.mughees;
 
-public class Main {
+import java.util.*;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+
+	    Scanner scanner = new Scanner(System.in);
+	    int totalPlayers = scanner.nextInt();
+	    ArrayList<Integer> playerScores = new ArrayList<>();
+	    for(int i = 0; i < totalPlayers; i++) {
+	        playerScores.add(scanner.nextInt());
+        }
+
+	    int aliceTotalScores = scanner.nextInt();
+	    ArrayList<Integer> aliceArrayList = new ArrayList<>();
+	    for(int i = 0; i < aliceTotalScores; i++) {
+	        aliceArrayList.add(scanner.nextInt());
+        }
+
+        List<Integer> finalList = new ArrayList<>(playerScores);
+        for(int i = 0; i < aliceTotalScores; i++) {
+            finalList.add(aliceArrayList.get(i));
+            finalList.sort(Collections.reverseOrder());
+            Set<Integer> set = new LinkedHashSet<>(finalList);
+            finalList.clear();
+            finalList.addAll(set);
+            System.out.println(finalList.indexOf(aliceArrayList.get(i)) + 1);
+        }
     }
 }
