d3a1dfc9815ed8be6269ed4e5e475922dd240329
mughees-asif <mughees460@gmail.com>
2020-06-19 15:31:20 +0100

    ClimbingTheLeaderboard completed

--- algorithms/ClimbingTheLeaderboard/src/com/mughees/Main.java
+++ algorithms/ClimbingTheLeaderboard/src/com/mughees/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
 	    Scanner scanner = new Scanner(System.in);
-	    
+
 	    int totalPlayers = scanner.nextInt();
 	    ArrayList<Integer> playerScores = new ArrayList<>();
 	    for(int i = 0; i < totalPlayers; i++) {
