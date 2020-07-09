52aa9d9fcb2504b3e425cb566e1cd358bfc49980
mughees-asif <mughees460@gmail.com>
2020-07-09 14:46:00 +0100

    FlatLands Space Stations completed

--- algorithms/FlatLand/src/com/mughees/Main.java
+++ algorithms/FlatLand/src/com/mughees/Main.java
@@ -3,6 +3,38 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+    }
+
+    static int flatlandSpaceStations(int n, int[] c) {
+        int i = 0;
+        int m = c.length;
+        int max = 0;
+        int diff = 90;
+        int j, x;
+        int temp = 1;
+
+        for (i = 0; i < n; i++) {
+            temp = 1;
+            for (j = 0; j < m; j++) {
+                if (c[j] == i) {
+                    temp = 0;
+                    break;
+                }
+            }
+
+            if (temp != 0) {
+                diff = Math.abs(c[0] - i);
+                for (j = 1; j < m; j++) {
+                    x = Math.abs(c[j] - i);
+                    if (x < diff) {
+                        diff = x;
+                    }
+                }
+                if (diff > max)
+                    max = diff;
+            }
+        }
+        return max;
     }
 }
