387a5edf59ca420eb49757208131ae5afbc5df47
mughees-asif <mughees460@gmail.com>
2020-07-09 12:34:52 +0100

    ChocolateFeast compeleted

---
+++ algorithms/ChocolateFeast/src/com/mughees/Main.java
@@ -0,0 +1,29 @@
+package com.mughees;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = input.nextInt();
+            int c = input.nextInt();
+            int m = input.nextInt();
+
+            int ate = 0;
+
+            int chocolates = n / c;
+
+            ate += chocolates;
+
+            while (chocolates >= m) {
+                ate += chocolates / m;
+                chocolates = (chocolates / m) + (chocolates % m);
+            }
+            System.out.println(ate);
+        }
+    }
+}
