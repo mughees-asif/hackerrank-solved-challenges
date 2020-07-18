edbd0fd95b9e8504aeedc2b85402236ca08d0ee8
mughees-asif <mughees460@gmail.com>
2020-07-18 20:38:23 +0100

    HackerRank in a String completed

--- algorithms/HackerRankInString/src/com/mughees/Main.java
+++ algorithms/HackerRankInString/src/com/mughees/Main.java
@@ -1,13 +1,31 @@
 package com.mughees;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String s = "hereiamstackerrank";
+        String s2 = "hackerworld";
+        String s3 = "hhaacckkekraraannk";
+        String s4 = "rhbaasdndfsdskgbfefdbrsdfhuyatrjtcrtyytktjjt";
+
+        System.out.println(hackerrankInString(s));
+        System.out.println(hackerrankInString(s2));
+        System.out.println(hackerrankInString(s3));
+        System.out.println(hackerrankInString(s4));
+
     }
 
     static String hackerrankInString(String s) {
-
-
+        String name = "hackerrank";
+        if(s.length() < name.length())  return "NO";
+        int j = 0;
+        for(int i = 0 ; i < s.length(); i++) {
+            if(j < name.length() && s.charAt(i) == name.charAt(j)) {
+                j++;
+            }
+        }
+        return (j == name.length() ? "YES" : "NO");
     }
 }
