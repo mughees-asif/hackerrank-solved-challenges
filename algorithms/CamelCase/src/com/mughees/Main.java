68de0437dead79381925311642e79947ea6f4587
mughees-asif <mughees460@gmail.com>
2020-07-22 14:29:22 +0100

    Algorithms: TwoStrings

--- algorithms/CamelCase/src/com/mughees/Main.java
+++ algorithms/CamelCase/src/com/mughees/Main.java
@@ -1,14 +1,15 @@
 package com.mughees;
 
 import java.lang.reflect.Array;
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println(camelcase("saveChangesInTheEditor"));
+        System.out.println(camelCase("saveChangesInTheEditor"));
     }
 
+    // Java 8
     static int camelcase(String s) {
         String[] split = s.split("[A-Z]");
         int result = 0;
@@ -16,5 +17,11 @@ public class Main {
             result = i;
         }
         return result + 1;
+    }
+
+    // Java 11
+    static int camelCase(String s) {
+        String[] split = s.split("[A-Z]");
+        return Array.getLength(split);
     }
 }
