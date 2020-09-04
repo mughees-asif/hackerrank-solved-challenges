1a9af43bddd5e8c7cab1c38cf73cc0218bd2ff53
mughees-asif <mughees460@gmail.com>
2020-09-04 16:06:37 +0100

    ALGORITHMS: Triple Sum completed

--- algorithms/TripleSum/src/com/mughees/Main.java
+++ algorithms/TripleSum/src/com/mughees/Main.java
@@ -1,5 +1,6 @@
 package com.mughees;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -11,18 +12,43 @@ public class Main {
         int lengthB = scanner.nextInt();
         int lengthC = scanner.nextInt();
 
-        int[] arrA = new int[lengthA];
-        int[] arrB = new int[lengthB];
-        int[] arrC = new int[lengthC];
+        int[] arrA = createArr(lengthA);
+        int[] arrB = createArr(lengthB);
+        int[] arrC = createArr(lengthC);
 
-        for(int i = 0; i < lengthA; i++) {
-            arrA[i] = scanner.nextInt();
+        System.out.println(tripleSum(arrA, arrB, arrC));
+    }
+
+    static int[] createArr(int length) {
+        Scanner scanner = new Scanner(System.in);
+        int[] result = new int[length];
+        for (int i = 0; i < length; i++) {
+            result[i] = scanner.nextInt();
         }
-        for(int i = 0; i < lengthB; i++) {
-            arrB[i] = scanner.nextInt();
+        return result;
+    }
+
+    static long tripleSum(int[] arrA, int[] arrB, int[] arrC) {
+        int[] sortedArrA = getUniqueElements(arrA);
+        int[] sortedArrB = getUniqueElements(arrB);
+        int[] sortedArrC = getUniqueElements(arrC);
+
+        long result = 0;
+        for (int value : sortedArrB) {
+            result += (long) findUniqueElements(sortedArrA, value) * findUniqueElements(sortedArrC, value);
         }
-        for(int i = 0; i < lengthC; i++) {
-            arrC[i] = scanner.nextInt();
+        return result;
+    }
+
+    static int[] getUniqueElements(int[] arr) {
+        return Arrays.stream(arr).distinct().sorted().toArray();
+    }
+
+    static int findUniqueElements(int[] arr, int num) {
+        int index = Arrays.binarySearch(arr, num);
+        if (index < 0) {
+            index = -1 - index - 1;
         }
+        return index + 1;
     }
 }
