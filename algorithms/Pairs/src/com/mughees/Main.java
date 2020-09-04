3b74888799793a74f72db94bb76f82e489c6ce87
mughees-asif <mughees460@gmail.com>
2020-09-04 15:04:51 +0100

    ALGORITHMS: Pairs completed

--- algorithms/Pairs/src/com/mughees/Main.java
+++ algorithms/Pairs/src/com/mughees/Main.java
@@ -1,8 +1,34 @@
 package com.mughees;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int sizeOfArr = scanner.nextInt();
+        int targetValue = scanner.nextInt();
+        int[] arr = new int[sizeOfArr];
+        for (int i = 0; i < sizeOfArr; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        Arrays.sort(arr);
+        int count = 0;
+        int left = 0;
+        int right = 0;
+
+        while (right < sizeOfArr) {
+            if (arr[right] - arr[left] == targetValue) {
+                count++;
+                left++;
+                right++;
+            } else if (arr[right] - arr[left] > targetValue) {
+                left++;
+            } else {
+                right++;
+            }
+        }
+        System.out.println(count);
     }
 }
