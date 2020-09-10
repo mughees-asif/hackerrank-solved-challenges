e65d9630e90039c101e8ff376455f3ff7ff5e6a9
mughees-asif <mughees460@gmail.com>
2020-09-10 16:33:44 +0100

    ALGORITHMS: Bigger is greater

--- algorithms/BiggerIsGreater/src/com/mughees/Main.java
+++ algorithms/BiggerIsGreater/src/com/mughees/Main.java
@@ -1,7 +1,5 @@
 package com.mughees;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -15,13 +13,21 @@ public class Main {
 
         // start from the right most char to find the first char which
         // is smaller than the previous char
-        for(endIndex = length - 1; endIndex > 0; endIndex--) {
-            if(inputStringArr[endIndex] > inputStringArr[endIndex - 1]) {
+        for (endIndex = length - 1; endIndex > 0; endIndex--) {
+            if (inputStringArr[endIndex] > inputStringArr[endIndex - 1]) {
                 break;
             }
         }
+        
+        // if no such char is found, then all the chars are in descending order
+        // therefore there is no greater string with the same set of chars
 
-        // if no 
+        if (endIndex == 0) {
+            return "no answer";
+        } else {
+
+        }
+
 
         return w;
     }
