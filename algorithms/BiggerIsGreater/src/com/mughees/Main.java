b1ea610f1d599af4f26d9b8cbce7ca07c863904c
mughees-asif <mughees460@gmail.com>
2020-09-10 16:07:06 +0100

    ALGORITHMS: Bigger is greater

--- algorithms/BiggerIsGreater/src/com/mughees/Main.java
+++ algorithms/BiggerIsGreater/src/com/mughees/Main.java
@@ -5,4 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    static String biggerIsGreater(String w) {
+        
+    return "no answer";
+    }
 }
