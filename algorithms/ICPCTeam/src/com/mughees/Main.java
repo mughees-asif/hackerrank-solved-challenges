5ea88bbb7091f69c21281997e4d6e6e6138ec6f9
mughees-asif <mughees460@gmail.com>
2020-09-18 20:31:09 +0100

    Algorithms: ACM ICPC Team

--- algorithms/ICPCTeam/src/com/mughees/Main.java
+++ algorithms/ICPCTeam/src/com/mughees/Main.java
@@ -1,8 +1,35 @@
 package com.mughees;
 
+import java.math.BigInteger;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+    }
+
+    static int[] acmTeam(String[] topic) {
+        int n = topic.length;
+
+        BigInteger[] bi = new BigInteger[n];
+        for (int i = 0; i < n; i++)
+            bi[i] = new BigInteger(topic[i], 2);
+
+        int maxTopic = 0;
+        int teamCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                BigInteger iuj = bi[i].or(bi[j]);
+                int bitCount = iuj.bitCount();
+                if (bitCount > maxTopic) {
+                    maxTopic = bitCount;
+                    teamCount = 1;
+                } else if (bitCount == maxTopic) {
+                    teamCount++;
+                }
+            }
+        }
+        return new int[]{maxTopic, teamCount};
     }
 }
