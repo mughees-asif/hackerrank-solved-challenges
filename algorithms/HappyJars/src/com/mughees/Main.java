24bd7b2f5eb5d6ccd71c5f0bb274e144afcbc2b4
mughees-asif <mughees460@gmail.com>
2020-09-20 20:03:55 +0100

    Algorithms: Filling Jars

--- algorithms/HappyJars/src/com/mughees/Main.java
+++ algorithms/HappyJars/src/com/mughees/Main.java
@@ -1,5 +1,6 @@
 package com.mughees;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,13 +9,28 @@ public class Main {
 	    Scanner scanner = new Scanner(System.in);
 	    int numOfEmptyJars = scanner.nextInt();
 	    int operations = scanner.nextInt();
+	    int[] candyArray = new int[numOfEmptyJars];
 
 	    for(int i = 0; i < operations; i++) {
 	        int firstInput = scanner.nextInt();
 	        int secondInput = scanner.nextInt();
 	        int numOfCandiesAdded = scanner.nextInt();
+
+	        for(int j = firstInput; j < secondInput; j++) {
+				candyArray[j - 1] += numOfCandiesAdded;
+
+
+			}
         }
+		System.out.println(Arrays.toString(candyArray));
     }
+
+//	static int solve(int n, int[][] operations) {
+//		int average = 0;
+//		for(int wholeArrayIndex = 0; wholeArrayIndex < )
+//
+//
+//	}
 
 
 }
