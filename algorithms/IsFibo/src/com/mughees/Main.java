4ecc9a73a5ed35eb246b3921b520a59bb668320d
mughees-asif <mughees460@gmail.com>
2020-09-22 20:04:25 +0100

    ALGORITHMS: Is Fibo

--- algorithms/IsFibo/src/com/mughees/Main.java
+++ algorithms/IsFibo/src/com/mughees/Main.java
@@ -1,30 +1,27 @@
 package com.mughees;
+
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            double n = scanner.nextDouble();
+            System.out.println(solve(n));
+        }
+    }
 
     static String solve(double n) {
-        if(isPerfectSquare(5*n*n - 4) || isPerfectSquare(5*n*n + 4))
+        // Ira Gessels method to check if a number is 
+        if (isPerfectSquare(5 * n * n - 4) || isPerfectSquare(5 * n * n + 4))
             return "IsFibo";
         else
             return "IsNotFibo";
     }
 
-    static boolean isPerfectSquare(double x){
+    static boolean isPerfectSquare(double x) {
         double sqr = Math.sqrt(x);
         return (sqr == Math.floor(sqr));
-    }
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-
-        int t = scanner.nextInt();
-
-        while(t-- > 0){
-            double n = scanner.nextDouble();
-            System.out.println(solve(n));
-        }
-
     }
 }
