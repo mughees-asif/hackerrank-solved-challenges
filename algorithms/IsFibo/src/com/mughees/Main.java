09b68035f6a22f7a8414828c7b6c587d41b9b246
mughees-asif <mughees460@gmail.com>
2020-09-22 19:36:05 +0100

    ALGORITHMS: Is Fibo

--- algorithms/IsFibo/src/com/mughees/Main.java
+++ algorithms/IsFibo/src/com/mughees/Main.java
@@ -1,8 +1,40 @@
 package com.mughees;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int testCases = scanner.nextInt();
+        int numToCheck = 0;
+        for (int i = 0; i < testCases; i++) {
+            numToCheck = scanner.nextInt();
+
+            int[] fibonacciArray = fibonacciArray(numToCheck);
+            Arrays.sort(fibonacciArray);
+
+            int flag = Arrays.binarySearch(fibonacciArray, numToCheck);
+            if(flag > 1) {
+                System.out.println("IsFibo");
+            } else {
+                System.out.println("IsNotFibo");
+            }
+        }
     }
+
+    static int[] fibonacciArray(int input) {
+        if (input <= 1) {
+            return new int[]{input};
+        }
+        int[] result = new int[input + 1];
+        result[0] = 0;
+        result[1] = 1;
+        for (int i = 2; i <= input; i++) {
+            result[i] = result[i - 1] + result[i - 2];
+        }
+        return result;
+    }
+
 }
