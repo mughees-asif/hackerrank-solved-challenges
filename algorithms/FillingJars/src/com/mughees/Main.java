86a1c17f75fa4320c63856b57cae9e6183130b56
mughees-asif <mughees460@gmail.com>
2020-09-24 12:53:14 +0100

    ALGORITHMS: Fixed Filling Jars link

--- algorithms/FillingJars/src/com/mughees/Main.java
+++ algorithms/FillingJars/src/com/mughees/Main.java
@@ -1,8 +1,21 @@
 package com.mughees;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner reader = new Scanner(System.in);
+        long N = reader.nextInt();
+        long M = reader.nextInt();
+        long total = 0;
+        for (int i = 0; i < M; i++) {
+            long a = reader.nextInt();
+            long b = reader.nextInt();
+            long k = reader.nextInt();
+            total = total + (b - a + 1) * k;
+        }
+        long avg = (long) Math.floor(total / N);
+        System.out.println(avg);
     }
 }
