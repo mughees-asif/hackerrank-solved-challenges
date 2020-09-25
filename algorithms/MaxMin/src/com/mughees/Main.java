f3e552f64cf9ff891bf2ecd8289ac5241a355a4f
mughees-asif <mughees460@gmail.com>
2020-09-25 18:02:51 +0100

    ALGORITHMS: Max Min

--- algorithms/MaxMin/src/com/mughees/Main.java
+++ algorithms/MaxMin/src/com/mughees/Main.java
@@ -1,8 +1,27 @@
 package com.mughees;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Scanner scanner = new Scanner(System.in);
+	    int numOfElements = scanner.nextInt();
+	    int target = scanner.nextInt();
+	    int[] numArr = new int[numOfElements];
+	    for(int i = 0; i < numOfElements; i++) {
+	        numArr[i] = scanner.nextInt();
+        }
+        Arrays.sort(numArr);
+
+	    int[] maxArr = new int[target];
+		System.arraycopy(numArr, 0,maxArr, 0, maxArr.length - target);
+		int[] minArr = new int[target];
+
+		System.arraycopy(numArr, 0,minArr, 0, target);
+
+		System.out.println("maxArr" + Arrays.toString(maxArr));
+		System.out.println("minArr" + Arrays.toString(minArr));
     }
 }
