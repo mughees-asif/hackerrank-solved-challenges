d9c5a5566934e9ffc8005e1aff3d893abc9028ae
mughees-asif <mughees460@gmail.com>
2020-09-27 20:40:56 +0100

    ALGORTIHMS: Jim and the Order

--- algorithms/JimOrders/src/com/mughees/Main.java
+++ algorithms/JimOrders/src/com/mughees/Main.java
@@ -1,8 +1,17 @@
 package com.mughees;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+	    int testCases = scanner.nextInt();
+
+	    for(int i = 0; i < testCases; i++) {
+	        int customerNum = scanner.nextInt();
+	        int orderNum = scanner.nextInt();
+	        int result = customerNum + orderNum;
+        }
     }
 }
