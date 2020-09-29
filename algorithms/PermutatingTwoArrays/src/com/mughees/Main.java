d73fb72da6b0d18f539e296b32fc4922c259457f
mughees-asif <mughees460@gmail.com>
2020-09-29 20:06:00 +0100

    ALGORITHMS: Permutating Two Arrays

--- algorithms/PermutatingTwoArrays/src/com/mughees/Main.java
+++ algorithms/PermutatingTwoArrays/src/com/mughees/Main.java
@@ -1,8 +1,34 @@
 package com.mughees;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int testCases = scanner.nextInt();
+        int sizeOfArrays = scanner.nextInt();
+        int targetToFind = scanner.nextInt();
+        int[] arrayA = new int[sizeOfArrays];
+        int[] arrayB = new int[sizeOfArrays];
+
+        for (int i = 0; i < sizeOfArrays; i++) {
+            arrayA[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(arrayA));
+        for (int i = 0; i < sizeOfArrays; i++) {
+            arrayB[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(arrayB));
+
+        // rotate array
+        for(int i = 0; i < sizeOfArrays; i++) {
+
+        }
+
+
+
+
     }
 }
