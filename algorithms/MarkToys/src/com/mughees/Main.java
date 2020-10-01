f9b934893f58f594c4e05e2707425642d2ffce9f
mughees-asif <mughees460@gmail.com>
2020-10-01 21:55:40 +0100

    ALGORITHMS: Mark and toys

--- algorithms/MarkToys/src/com/mughees/Main.java
+++ algorithms/MarkToys/src/com/mughees/Main.java
@@ -6,28 +6,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    Scanner scanner = new Scanner(System.in);
-	    int numOfToys = scanner.nextInt();
-	    int moneyToSpend = scanner.nextInt();
-	    int[] toyPrices = new int[numOfToys];
-	    for(int i = 0; i < toyPrices.length; i++) {
-	        toyPrices[i] = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int numOfToys = scanner.nextInt();
+        int moneyToSpend = scanner.nextInt();
+        int[] toyPrices = new int[numOfToys];
+        for (int i = 0; i < toyPrices.length; i++) {
+            toyPrices[i] = scanner.nextInt();
         }
         Arrays.sort(toyPrices);
-	    
-	    int firstPointer = 0;
-	    int secondPointer = 1;
-	    int max = 0;
-	    int sum = 0;
 
-	    for(int i = 0; i < toyPrices.length; i++) {
-	        sum += toyPrices[firstPointer] + toyPrices[secondPointer];
-	        if(sum < moneyToSpend) {
-	            secondPointer++;
-	            max++;
-            } else if (sum == moneyToSpend) {
-                System.out.println(max);
+        int firstPointer = 0;
+        int secondPointer = 1;
+        int max = 1;
+        int sum = toyPrices[firstPointer];
+
+        if (sum >= moneyToSpend) {
+            System.out.println("0");
+        } else {
+            for (int i = 0; i < toyPrices.length; i++) {
+                sum += toyPrices[secondPointer];
+                if (sum <= moneyToSpend) {
+                    secondPointer++;
+                    max++;
+                }
             }
         }
+        System.out.println(max);
     }
 }
+
