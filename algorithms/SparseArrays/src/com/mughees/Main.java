d0b88b708bd9a98af250a26b508be5cfd7fbd276
mughees-asif <mughees460@gmail.com>
2020-11-13 20:26:44 +0000

    MEDIUM_ARRAYS: Spare Arrays

--- algorithms/SparseArrays/src/com/mughees/Main.java
+++ algorithms/SparseArrays/src/com/mughees/Main.java
@@ -1,24 +1,33 @@
 package com.mughees;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(matchingStrings(new String[]{"aba", "baba", "aba", "xzxb"}, new String[]{"aba", "ab", "xzxb"})));
+        System.out.println(Arrays.toString(matchingStrings(new String[]{"aba", "baba", "aba", "xzxb"}, new String[]{"aba", "xzxb", "ab"})));
+        System.out.println(Arrays.toString(matchingStrings(new String[]{"aba", "baba", "aba", "xzxb"}, new String[]{"aba", "xzxb", "ab"})));
     }
 
     static int[] matchingStrings(String[] strings, String[] queries) {
         int counter = 0;
         int[] result = new int[queries.length];
 
-        HashMap<String, Integer> map = new HashMap<>();
-        for(String word : strings) {
+        LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
+        for (String word : strings) {
             map.put(word, map.getOrDefault(word, 0) + 1);
         }
         System.out.println(map);
 
+        List<Integer> valueList = new ArrayList<>(map.values());
+        System.out.println(valueList);
+        for (int i = 0; i < queries.length; i++) {
+            for (String word : map.keySet()) {
+                if (word.equals(queries[i])) {
+                    result[i] = valueList.get(i);
+                }
+            }
+        }
 
         return result;
     }
