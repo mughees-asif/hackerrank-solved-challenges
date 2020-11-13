73a46a34b674905d83c4ed3c7afe50de64346fd6
mughees-asif <mughees460@gmail.com>
2020-11-13 20:37:30 +0000

    MEDIUM_ARRAYS: Spare Arrays

--- algorithms/SparseArrays/src/com/mughees/Main.java
+++ algorithms/SparseArrays/src/com/mughees/Main.java
@@ -10,20 +10,25 @@ public class Main {
     }
 
     static int[] matchingStrings(String[] strings, String[] queries) {
-        int[] result = new int[queries.length];
-        LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
-        for (String word : strings) {
-            map.put(word, map.getOrDefault(word, 0) + 1);
-        }
-        List<Integer> valueList = new ArrayList<>(map.values());
-        for (int i = 0; i < queries.length; i++) {
-            for (String word : map.keySet()) {
-                if (word.equals(queries[i])) {
-                    result[i] = valueList.get(i);
-                    break;
-                }
+        Map<String, Integer> map = new HashMap<>();
+        int result[] = new int[queries.length];
+
+        for (int i = 0; i < strings.length; i++) {
+            String inputString = strings[i];
+            if (map.containsKey(inputString)) {
+                map.put(inputString, map.get(inputString) + 1);
+            } else {
+                map.put(inputString, 1);
             }
         }
+
+        for (int i = 0; i < queries.length; i++) {
+            String queryString = queries[i];
+            if (map.containsKey(queryString)) {
+                result[i] = map.get(queryString);
+            }
+        }
+
         return result;
     }
 
