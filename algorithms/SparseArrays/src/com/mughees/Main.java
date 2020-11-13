756e4c07bcd85d86d221f415b58f5015b7503291
mughees-asif <mughees460@gmail.com>
2020-11-13 20:14:40 +0000

    MEDIUM_ARRAYS: Spare Arrays

--- algorithms/SparseArrays/src/com/mughees/Main.java
+++ algorithms/SparseArrays/src/com/mughees/Main.java
@@ -1,8 +1,25 @@
 package com.mughees;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(Arrays.toString(matchingStrings(new String[]{"aba", "baba", "aba", "xzxb"}, new String[]{"aba", "ab", "xzxb"})));
+    }
+
+    static int[] matchingStrings(String[] strings, String[] queries) {
+        int counter = 0;
+        int[] result = new int[queries.length];
+
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String word : strings) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        System.out.println(map);
+
+
+        return result;
     }
 }
