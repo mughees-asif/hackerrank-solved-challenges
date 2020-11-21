030305f8bdd40a4cc73d082c50cdefe79cef62c5
mughees-asif <mughees460@gmail.com>
2020-11-21 12:54:49 +0000

    DS: Tree Traversals

--- algorithms/TreeTraversal/src/com/mughees/Node.java
+++ algorithms/TreeTraversal/src/com/mughees/Node.java
@@ -5,17 +5,25 @@ class Node {
     private Node left, right;
 
     void preOrder(Node root) {
-
+        if(root == null)    return;
+        System.out.println(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
     }
 
     void postOrder(Node root) {
-
+        if(root == null)    return;
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println(root.data + " ");
     }
 
     void inOrder(Node root) {
-
+        if(root == null)    return;
+        inOrder(root.left);
+        System.out.println(root.data + " ");
+        inOrder(root.right);
     }
-
 }
 
 
