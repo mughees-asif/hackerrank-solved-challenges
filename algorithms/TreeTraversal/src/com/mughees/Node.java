167c038cf79ddcc4ba68bc04218945983e88321e
mughees-asif <mughees460@gmail.com>
2020-11-21 12:50:23 +0000

    DS: Tree Traversals

---
+++ algorithms/TreeTraversal/src/com/mughees/Node.java
@@ -0,0 +1,22 @@
+package com.mughees;
+
+class Node {
+    private int data;
+    private Node left, right;
+
+    void preOrder(Node root) {
+
+    }
+
+    void postOrder(Node root) {
+
+    }
+
+    void inOrder(Node root) {
+
+    }
+
+}
+
+
+
