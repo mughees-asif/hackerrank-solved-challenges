98e30b7aa1352bb64babb8738d191ca9f3138f2a
mughees-asif <mughees460@gmail.com>
2020-11-26 20:05:42 +0000

    DS: Tree Insertion

---
+++ algorithms/TreeInsertion/src/Node.java
@@ -0,0 +1,25 @@
+public class Node {
+    Node left, right;
+    int data;
+
+    private Node(int item) {
+        key = item;
+        left = right = null;
+    }
+
+    private Node insertNode(Node root, int key) {
+        // base case: root node is null
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        // boolean check to see which leaf to insert new node
+        if(key < root.data) {
+            root.left = insertNode(root.left, key);
+        } else if (key > root.data) {
+            root.right = insertNode(root.right, key);
+        }
+        return root;
+    }
+}
