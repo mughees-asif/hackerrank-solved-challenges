42c773c092af38b9a79c478ca1602e73840b2978
mughees-asif <mughees460@gmail.com>
2020-11-26 20:29:09 +0000

    DS: Tree Search

--- algorithms/TreeSearch/src/Node.java
+++ algorithms/TreeSearch/src/Node.java
@@ -1,2 +1,20 @@
-public class Node {
+class Node {
+    private Node left, right;
+    private int data;
+
+    Node(int key) {
+        this.data= key;
+        left = null;
+        right = null;
+    }
+
+    private Node search(Node root, int key) {
+        if(root == null | root.data == key) {
+            return root;
+        }
+        if(root.data < key) {
+            return search(root.right, key);
+        }
+        return search(root.left, key);
+    }
 }
