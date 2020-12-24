acf359c7d6d825f6ccced3cb9d45ffb6905d6720
mughees-asif <mughees460@gmail.com>
2020-12-24 14:40:58 +0000

    Recursion: Balanced Brackets

--- algorithms/BalancedBrackets/src/com/mughees/Main.java
+++ algorithms/BalancedBrackets/src/com/mughees/Main.java
@@ -1,8 +1,31 @@
 package com.mughees;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(validParanthesis("{}()[]"));
+    }
+
+    private static boolean validParanthesis(String input) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (int i = 0; i < input.length(); i++) {
+            if(input.charAt(i) == '(' || input.charAt(i) == '{' || input.charAt(i) == '[') {
+                deque.add(input.charAt(i));
+            } else {
+                if(deque.isEmpty()) {
+                    return false;
+                }
+                if(input.charAt(i) == '(' && deque.peek() != ')' ||
+                        input.charAt(i) == '{' && deque.peek() != '}'||
+                        input.charAt(i) == '[' && deque.peek() != '}') {
+                    return false;
+                }
+                deque.pop();
+            }
+        }
+        return deque.isEmpty();
     }
 }
