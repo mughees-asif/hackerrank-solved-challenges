bf9edf5377491ebe5264ce06ce6eb4947a972a60
mughees-asif <mughees460@gmail.com>
2020-12-28 19:20:40 +0000

    MEDIUM_DS: Tree Diameter

--- algorithms/BinaryTreeDiameter/src/Node.java
+++ algorithms/BinaryTreeDiameter/src/Node.java
@@ -2,11 +2,24 @@ public class Node {
     private Node left, right;
     private int data;
 
-    public Node() {}
+    Node(int key) {
+        this.data = key;
+    }
 
-    public Node(Node root) { this.root = }
+    private int diameter(Node root) {
+        if (root == null) return 0;
+        // heights
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        // diameters
+        int diameterLeft = diameter(root.left);
+        int diameterRight = diameter(root.right);
 
+        return Math.max(leftHeight + rightHeight + 1, Math.max(diameterLeft, diameterRight));
+    }
 
-
-
+    private int height(Node root) {
+        if (root == null) return 0;
+        return Math.max(1 + height(root.left), height(root.right));
+    }
 }
